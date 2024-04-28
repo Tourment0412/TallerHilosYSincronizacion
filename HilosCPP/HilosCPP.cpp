477c047331b1744e1517b7478e95e7a5f32081b0
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-04-27 22:23:49 -0500

    Added some documentation

--- HilosCPP/HilosCPP.cpp
+++ HilosCPP/HilosCPP.cpp
@@ -1,3 +1,4 @@
+//Inclusión de librerias estandár para el programa
 #include <iostream>
 #include <thread>
 #include <queue>
@@ -5,26 +6,37 @@
 #include <condition_variable>
 #include <chrono>
 
+//Declaracion de variables globales del programa
 std::queue<int> cola;
 std::mutex mtx;
 std::condition_variable cv;
 bool produccion_terminada = false;
-
 int tiempoProductor;
 int tiempoConsumidor;
-
 int elementos_consumidos = 0;
 
+//Declaracion de la clase semaforo para la sincronizacion
 class Semaphore {
+/* Declaracion de sus elementos publicos que seran accesibles
+ * por las diferentes partes del programa
+ */
 public:
+    /* Constructor de la clase, recibe un entero que en caso de no enviarse
+     * se asignara como un cero
+     * */
     Semaphore(int count = 0) : count_(count) {}
 
+    /* Funcion que se usa para notificar la posibilidad de continuar
+     * la ejecucion de algun hilo que esta esperando
+     * */
     void notify() {
         std::unique_lock<std::mutex> lock(mutex_);
         ++count_;
         cv_.notify_one();
     }
-
+    /* Funcion que se usa para hacer esperar a un hilo con la instruccion wait
+     * En dado caso que no hayan instrucciones de que se puede ejecutar
+     */
     void wait() {
         std::unique_lock<std::mutex> lock(mutex_);
         while (count_ == 0) {
@@ -32,7 +44,7 @@ public:
         }
         --count_;
     }
-
+//Declaracion de los elementos privados de la clase Semaphore
 private:
     std::mutex mutex_;
     std::condition_variable cv_;
@@ -41,6 +53,9 @@ private:
 
 Semaphore semaforo(0);
 
+/* Metodo encargado de producir los elementos y notificar posibilidad de ejecucion
+ * al hilo consumidor cuando hayan productos disponibles
+ */
 void productor() {
     for (int i = 1; i <= 30; ++i) {
         {
@@ -53,11 +68,14 @@ void productor() {
     produccion_terminada = true;
     semaforo.notify();
 }
-
+/* Metodo que consumira los productos siempre y cuando la cola no este vacia
+ * esto se logra mediante la sincroninzacion con el producto.
+ */
 void consumidor() {
     while (true) {
         std::cout << "Consumidor: Esperando elemento" << std::endl;
         semaforo.wait();
+        std::cout << "Consumidor: Espera terminada"<< std::endl;
         if (produccion_terminada && cola.empty()) {
             break;
         }
@@ -74,7 +92,9 @@ void consumidor() {
         std::this_thread::sleep_for(std::chrono::milliseconds(tiempoConsumidor)); 
     }
 }
-
+/* Metodo main donde se inicializan los hilos y
+ * se espera a que finalicen su ejecucion
+ */
 int main() {
     std::cout<<"Ingrese el tiempo de productor (milisegundos): ";
     std::cin >> tiempoProductor;
@@ -83,9 +103,7 @@ int main() {
     std::cout << "\n";
     std::thread productor_thread(productor);
     std::thread consumidor_thread(consumidor);
-
     productor_thread.join();
     consumidor_thread.join();
-
     return 0;
 }
